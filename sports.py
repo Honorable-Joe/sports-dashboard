736f969ede39444f4aa6207d7653179222cfd1a6
Honorable-Joe <modyrock88@gmail.com>
2026-08-17 20:00:08 +0300

    Add files via upload

---
+++ sports.py
@@ -0,0 +1,11 @@
+SPORTS = {
+    'Soccer':'field', 'Swimming':'water', 'Boxing':'combat', 'MMA':'combat', 'Karate':'combat',
+    'Tennis':'racket', 'Basketball':'court', 'Handball':'court', 'Bodybuilding':'strength', 'General Fitness':'general'
+}
+POSITIONS = {
+    'Soccer':['Goalkeeper','Center Back','Full Back','Defensive Midfielder','Central Midfielder','Winger','Attacking Midfielder','Striker'],
+    'Swimming':['Freestyle','Backstroke','Breaststroke','Butterfly','Individual Medley'],
+    'Boxing':['General'], 'MMA':['General'], 'Karate':['Kumite','Kata','Kumite + Kata'],
+    'Tennis':['Singles','Doubles'], 'Basketball':['Point Guard','Shooting Guard','Small Forward','Power Forward','Center'],
+    'Handball':['Goalkeeper','Wing','Backcourt','Pivot'], 'Bodybuilding':['Off-season','Pre-contest'], 'General Fitness':['General']
+}
